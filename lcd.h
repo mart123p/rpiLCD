d710f923d71c8b0750ff43acec38a0f5afa5bf04
Martin Pouliot <contact@martinpouliot.ca>
2017-02-12 14:56:00 -0500

    Added some domumentation

--- lcd.h
+++ lcd.h
@@ -1,7 +1,11 @@
-//
-// Created by martin on 1/27/17.
-//
-
+/**
+ * @file
+ * @author Martin Pouliot
+ * @version 0.1dev
+ * @section DESCRIPTION
+ * 
+ * This class takes care of displaying text on the LCD attached to the Raspberry Pi, It is pretty basic as it only includes 3 methods. 
+ */
 #ifndef PILCD_LCD_H
 #define PILCD_LCD_H
 #define I2C_ADDR 0x27
@@ -19,8 +23,24 @@
 
 class LCD {
 public:
+    /**
+     * Initialises the lcd screen and the I2C interface. We are using device 1 since it is the I2C pins on the Raspberry Pi. However this might change if we change the version of the Raspberry pi.
+     * @param addr A byte for the I2C address
+     * @param lcd_cols A byte for the number of columns that the screen has
+     * @param lcd_rows A byte for the number of rows that the screen has
+     */
     void begin(uint8_t addr, uint8_t lcd_cols, uint8_t lcd_rows);
+
+    /**
+     * Erases the content of the screen
+     */
     void clear();
+
+    /**
+     * Prints a message to the screen.
+     * @param str The message to print to the screen
+     * @param pos Where do we print our message. Accepted values(LCD_LINE_1,LCD_LINE_2,LCD_LINE_3,LCD_LINE_4)
+     */
     void print(std::string str, uint8_t pos);
 
 private:
